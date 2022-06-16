c92bd8a6b99f4c026486c45998bdc92d5c3973a8
Maro-MN <107557717+Maro-MN@users.noreply.github.com>
2022-06-16 11:58:59 +0300

    Initial commit

---
+++ src/main/java/com/store/controller/StoreController.java
@@ -0,0 +1,16 @@
+package com.store.controller;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping(value="store")
+public class StoreController {
+
+	@RequestMapping(value="/home")
+	public ModelAndView mainPage(ModelMap model) {
+		return new ModelAndView("html/home");
+	}
+}
---
+++ src/test/java/com/app/store/StoreApplicationTests.java
@@ -0,0 +1,15 @@
+package com.app.store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import com.store.StoreApplication;
+
+@SpringBootTest(classes=StoreApplication.class)
+class StoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
